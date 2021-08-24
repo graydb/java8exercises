bc5d06ebc95628c17cebb692742d32f3e594776c
graydb <douglas.gray11@outlook.com>
2021-08-24 14:56:15 +0100

    DG:commit Exercise 1

--- src/main/java/exercise1/HelloWorld.java
+++ src/main/java/exercise1/HelloWorld.java
@@ -1,5 +1,28 @@
 package exercise1;
 
 public class HelloWorld {
+	
 
-}
+	            public static void main(String[] args) {
+
+	                           System.out.println("Hello World!");
+	                           String message = "Hello World!";
+	                           System.out.println(message);
+	                           printMessage();
+	                           printMessage();
+	                           printMessage();
+	                           printMessage();
+	              }
+
+	              public static void printMessage() {
+	                           String message = "Hello Dougie";
+	                           System.out.println(message);
+	              }
+
+	}
+
+	
+	
+	
+	
+
