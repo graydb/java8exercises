7cd10d43ecfab65aa1fbb298a96ba52e9bf4ca50
graydb <douglas.gray11@outlook.com>
2021-08-24 16:36:04 +0100

    DG commit exercise 2

--- src/main/java/exercise2/CalculatorTask.java
+++ src/main/java/exercise2/CalculatorTask.java
@@ -1,5 +1,33 @@
 package exercise2;
 
 public class CalculatorTask {
-
+	public static void main (String[] args) {
+		System.out.println(addTwo(8087d, 12345d));
+		System.out.println(mulTwo(123d, 56d));
+		System.out.println(minTwo(5653723d, 767d));
+		divTwo (8d,4d);
+		
+	}
+	
+	   public static double addTwo(double num1, double num2) {
+        return num1 + num2;
+    }
+       public static double minTwo(double num1, double num2) {
+        return num1 - num2;
+    }
+       public static double mulTwo(double num1, double num2) {
+           return num1 * num2;
+       }
+       
+       public static void divTwo(double num1, double num2) {
+        if (num1 < num2) {
+        	System.out.println ("Division by zero cannot be completed");
+        } else {
+        System.out.println(num1 / num2);
+    }
+          
 }
+
+	
+}
+
