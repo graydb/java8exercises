36da68dadd71487eaa746daf02be87d174333cea
graydb <douglas.gray11@outlook.com>
2021-08-25 09:32:05 +0100

    DG:Files created oop-1

--- src/main/java/exercise3/CheckString.java
+++ src/main/java/exercise3/CheckString.java
@@ -1,5 +1,20 @@
 package exercise3;
 
-public class CheckString {
 
-}
+		public class CheckString {
+			private static final String check = "what do you think";
+			CheckString() {};
+			public boolean GuessAgain(String guess) {
+				if (guess.equals(check)) return true; else return false;
+			}
+			public boolean Compare(String str1, String str2) {
+				if (str1.equals(str2)) return true; else return false;
+			}
+		
+		
+		
+		
+		
+	}
+
+
